f281c98b3ad22390fcc42d49714b83a6481cf5ef
bavadharanibalaji <bavadharanibalaji08@gmail.com>
2026-09-01 18:40:56 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-management-project-k3j1.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
